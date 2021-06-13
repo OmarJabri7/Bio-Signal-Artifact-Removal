5487fa73c129c9e81dc0655798bae9761c7c9e46
OmarJabri7 <omarjabri967@gmail.com>
2021-06-14 00:05:15 +0300

    Added new Dynamic Time Warping technique to asses difference between eeg and fake one. Got to 42 in difference units

--- sin2eeg.py
+++ sin2eeg.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.core.arrayprint import format_float_positional
 import scipy.signal as sig
 import pandas as pd
 from eeg import EEG
 from scipy.signal import butter, lfilter
 from scipy.io import loadmat
-from scipy.interpolate import interp1d
 from math import floor
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
 
 pi = np.pi
+FILT_ORDER = 2
 
 
 def butter_bandpass(signal, lowcut, highcut, fs, order=5):
@@ -34,12 +35,13 @@ dt = t[1] - t[0]
 fs = int(1/dt)  # Sampling frequency
 alpha = signal[:, 0][:N]  # Alpha wave
 # Band Pass Filter between 8-12 Hz
-filt_alpha = butter_bandpass(alpha, 8, 12, fs)
+filt_alpha = butter_bandpass(alpha, 8, 12, fs, order=FILT_ORDER)
 nfft = len(filt_alpha)
 fft_alpha = np.fft.fft(filt_alpha)  # FFT of signal
 alpha_amp = 2*np.abs(fft_alpha)/N  # Amplitude of signal
 # Frequencies on axis in Hz of signal
 alpha_hz = np.linspace(0, fs/2, floor(N/2) + 1)
+alpha_phi = 2*np.angle(fft_alpha)/N  # Phase of EEG Alpha wave
 # %%
 plt.plot(t, filt_alpha)
 plt.title("Real EEG (FP1)")
@@ -59,29 +61,29 @@ plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power spectral density (V^2 / Hz)')
 plt.figure()
 # %%
-f_start = 0
-f_end = 50
+f_start = 50
+f_end = 60
 f_tot = np.linspace(f_start, f_end, N)
 f_final = np.cumsum(f_tot/fs)
-MAX_AMP = 5000  # max amp
+MAX_AMP = 1e4  # max amp
 A = MAX_AMP*np.exp(-t)
-f = 8
+f = 7
 A = A * np.sin(2*np.pi*f/fs)
 phi = 0
 eeg_sig = EEG(A, np.sin, pi, f_final, t, phi)
 eeg_cos = EEG(A, np.cos, pi, f_final, t, phi)
 # %%
-f = 10
-sines = 1.2 * np.sin(2 * pi * f * t + 1.2) + 1.4 * np.sin(2 *
-                                                          pi * f * t + 0.3) + 0.9 * np.sin(2 * pi * f * t - 0.45)
+f_sin = 10
+sines = 1.2 * np.sin(2 * pi * f_sin * t + 1.2) + 1.4 * np.sin(2 *
+                                                              pi * f_sin * t + 0.3) + 0.9 * np.sin(2 * pi * f_sin * t - 0.45)
 noise_amp = 5
 gauss_noise = noise_amp*np.random.normal(0, 1, len(eeg_sig.signal))
 fake_eeg = (eeg_sig.signal) + sines + gauss_noise
 low_cut = 8
 high_cut = 12
-filt_eeg = butter_bandpass(fake_eeg, low_cut, high_cut, fs, order=4)
 eeg_dat = np.fromfile('subj27/blink.dat')
-filt_eeg = filt_eeg + eeg_dat[:len(filt_eeg)]
+filt_eeg = fake_eeg + eeg_dat[:len(fake_eeg)]
+filt_eeg = butter_bandpass(filt_eeg, low_cut, high_cut, fs, order=FILT_ORDER)
 nfft = len(filt_eeg)
 fft_eeg = np.fft.fft(filt_eeg)  # FFT of signal
 eeg_amp = 2*np.abs(filt_eeg)/N  # Amplitude of signal
@@ -104,8 +106,10 @@ plt.title("Fake EEG FR")
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power spectral density (V^2 / Hz)')
 plt.show()
-print(np.linalg.norm(psd_1, psd_2))
+distance, path = fastdtw(psd_1, psd_2, dist=euclidean)
+print(f"Difference btw signals: {distance}")
 # %%
+# TODO: Compare with other EEG signals.
 # eeg_data = pd.read_csv('EEG_sample.csv')
 # sensors = np.unique(eeg_data["sensor position"])
 # FP1 = eeg_data.loc[eeg_data['sensor position'] == "F7"]
