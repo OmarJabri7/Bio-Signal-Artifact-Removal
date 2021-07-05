33e6aa26efd6b97dd13c7472921b728d0d2fa2ff
OmarJabri7 <omarjabri967@gmail.com>
2021-07-05 03:04:53 +0300

    added a cuztomized version of the DNF

---
+++ deepNeuronalFilter/CMakeFiles/eeg_filter.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/omar/Desktop/UofG/Master's Project/Thesis/EEG-Artifact-Removal---UofG-Thesis/deepNeuronalFilter/dynamicPlots.cpp" "CMakeFiles/eeg_filter.dir/dynamicPlots.cpp.o" "gcc" "CMakeFiles/eeg_filter.dir/dynamicPlots.cpp.o.d"
+  "/Users/omar/Desktop/UofG/Master's Project/Thesis/EEG-Artifact-Removal---UofG-Thesis/deepNeuronalFilter/eeg_filter_demo.cpp" "CMakeFiles/eeg_filter.dir/eeg_filter_demo.cpp.o" "gcc" "CMakeFiles/eeg_filter.dir/eeg_filter_demo.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/omar/Desktop/UofG/Master's Project/Thesis/EEG-Artifact-Removal---UofG-Thesis/deepNeuronalFilter/DNF/CMakeFiles/CLDL.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ deepNeuronalFilter/CMakeFiles/eeg_filter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eeg_filter.dir/dynamicPlots.cpp.o"
+  "CMakeFiles/eeg_filter.dir/dynamicPlots.cpp.o.d"
+  "CMakeFiles/eeg_filter.dir/eeg_filter_demo.cpp.o"
+  "CMakeFiles/eeg_filter.dir/eeg_filter_demo.cpp.o.d"
+  "eeg_filter"
+  "eeg_filter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eeg_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
