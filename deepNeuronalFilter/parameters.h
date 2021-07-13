46c09dc2049b577af2453065a75b9ed1ae74c153
OmarJabri7 <omarjabri967@gmail.com>
2021-07-13 01:15:46 +0100

    added yet optimal parameters for nn

--- deepNeuronalFilter/parameters.h
+++ deepNeuronalFilter/parameters.h
@@ -14,26 +14,28 @@
 // NOISE:
 #define doOuterPreFilter
 #define doOuterDelayLine
-#define outerDelayLineLength 250 // CHange this
+#define outerDelayLineLength 300 // CHange this
 
 // SIGNAL:
 #define doInnerPreFilter
 #define doInnerDelay
-#define innerDelayLineLength 250 // Change this
+#define innerDelayLineLength 300 // Change this
 
 //NN specifications
 #define DoDeepLearning
-#define NLAYERS 11
+#define NLAYERS 13
+#define N12 37 //12
+#define N11 32 //11
 #define N10 29 //10
-#define N9 23 //9
-#define N8 19 //8
-#define N7 17 //7
-#define N6 13 //6
-#define N5 11 //5
-#define N4 7 //4
-#define N3 5 //3
-#define N2 3 //2
-#define N1 2 //1
-#define N0 1 //this has to always be 1
+#define N9 23  //9
+#define N8 19  //8
+#define N7 17  //7
+#define N6 13  //6
+#define N5 11  //5
+#define N4 7   //4
+#define N3 5   //3
+#define N2 3   //2
+#define N1 2   //1
+#define N0 1   //this has to always be 1
 
 #endif //EEGFILTER_PARAMETERS_H
